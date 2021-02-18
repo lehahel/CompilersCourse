4ba39396c23f77bedeb8eaedd7be2296e349d2bf
lehahel <lehahel2014@gmail.com>
2021-02-19 00:58:45 +0300

    added expressions

---
+++ expression/addexpression.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <memory>
+#include "expression.h"
+
+template <typename T, typename Left, typename Right>
+class CAddExpression : public CExpression<T> {
+public:
+    CAddExpression(std::shared_ptr<Left>  left,
+                   std::shared_ptr<Right> right);
+    T eval() const override;
+
+private:
+    std::shared_ptr<CExpression<Left>>  left;
+    std::shared_ptr<CExpression<Right>> right;
+};
---
+++ parser_new.y
@@ -0,0 +1,8 @@
+%skeleton "lalr1.cc"
+%require "3.4"
+
+%defines
+%define api.token.constructor
+%define api.value.type variant
+%define parse.assert
+
